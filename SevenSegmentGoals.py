073862bd6277cc6a55021b76beb53643ed423150
andreas riedmann <andreas.riedmann@htldornbirn.at>
2024-10-21 16:21:30 +0200

    fixed goals

--- SevenSegmentGoals.py
+++ SevenSegmentGoals.py
@@ -25,73 +25,30 @@ class SevenSegmentGoals:
        
     def printToSegment(self, goals1, goals2 ):
         self.segment1.fill(0)
+        self.segment2.fill(0)
         # Set the first character to '1':
         if (goals1<10):
             #self.segment1[0] = str(int(goals1 / 10))
             self.segment1[1] = str(goals1)
+            self.segment2[1] = str(goals1)
         # Set the second character to '2':
         if (goals1>9):
             self.segment1[0] = str(int(goals1 / 10))
             self.segment1[1] = str(goals1 % 10)
+            self.segment2[0] = str(int(goals1 / 10))
+            self.segment2[1] = str(goals1 % 10)
         # Set the third character to 'A':
         if (goals2<10):
             self.segment1[2] = str(int(goals1))
+            self.segment2[2] = str(int(goals1))
         # Set the forth character to 'B':
         if (goals1>9):
             self.segment1[2] = str(int(goals1 / 10))
             self.segment1[3] = str(goals1 % 10)
+            self.segment2[2] = str(int(goals1 / 10))
+            self.segment2[3] = str(goals1 % 10)
         self.segment1.print(':')
+        self.segment2.print(':')
 
         self.segment2.fill(0)
-        # Set the first character to '1':
-        self.segment2[0] = str(int(goals1 / 10))
-        # Set the second character to '2':
-        self.segment2[1] = str(goals1 % 10)
-        # Set the third character to 'A':
-        self.segment2[2] = str(int(goals1 / 10))
-        # Set the forth character to 'B':
-        self.segment2[3] = str(goals1 % 10)
-        self.segment2.print(':')
-        # self.segment1.clear()
-        # if goals1 < 10:
-        #     #self.segment.set_digit(0, int(goals1 / 10))     # Tens
-        #     self.segment1.set_digit(1, goals1 % 10)          # Ones
-        # else:
-        #     self.segment1.set_digit(0, int(goals1 / 10))     # Tens
-        #     self.segment1.set_digit(1, goals1 % 10)          # Ones
-
-        # # Set minutes
-        # if goals2 < 10:
-        #     #self.segment.set_digit(2, int(goals2 / 10))   # Tens
-        #     self.segment1.set_digit(2, goals2 % 10)        # Ones
-        # else:
-        #     self.segment1.set_digit(2, int(goals2 / 10))   # Tens
-        #     self.segment1.set_digit(3, goals2 % 10)   
-        # # Toggle colon
-        # self.segment1.set_colon(1)              # Toggle colon at 1Hz
-        # # Write the display buffer to the har     dware.  This must be called to
-        # # update the actual display LEDs.
-        # self.segment1.write_display()
-    
-        # self.segment2.clear()
-        # if goals1 < 10:
-        #     #self.segment.set_digit(0, int(goals1 / 10))     # Tens
-        #     self.segment2.set_digit(1, goals1 % 10)          # Ones
-        # else:
-        #     self.segment2.set_digit(0, int(goals1 / 10))     # Tens
-        #     self.segment2.set_digit(1, goals1 % 10)          # Ones
-
-        # # Set minutes
-        # if goals2 < 10:
-        #     #self.segment.set_digit(2, int(goals2 / 10))   # Tens
-        #     self.segment2.set_digit(2, goals2 % 10)        # Ones
-        # else:
-        #     self.segment2.set_digit(2, int(goals2 / 10))   # Tens
-        #     self.segment2.set_digit(3, goals2 % 10)   
-        # # Toggle colon
-        # self.segment2.set_colon(1)              # Toggle colon at 1Hz
-        # # Write the display buffer to the har     dware.  This must be called to
-        # # update the actual display LEDs.
-        # self.segment2.write_display()
-    
-   
+       
