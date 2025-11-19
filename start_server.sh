62045ffb06430b3ffad5a3b1d2e760c92d278b5c
andreas riedmann <andreas.riedmann@htldornbirn.at>
2025-11-19 10:49:26 +0100

    added start script

---
+++ start_server.sh
@@ -0,0 +1,29 @@
+  GNU nano 7.2                                                                                             start_server_and_browser.sh                                                                                                      
+#!/bin/bash
+# Activate virtual environment if needed
+source /home/pi/Documents/kickerkasten/venv/bin/activate
+
+# Start the Flask server in background
+python3 /home/pi/Documents/kickerkasten/server.py &
+
+# Give the server a few seconds to start
+sleep 5
+
+# Set DISPLAY variable if not already set
+export DISPLAY=${DISPLAY:-:0}
+
+# Check if X server is running before launching browser
+if xset q &>/dev/null; then
+    # Open Chromium in kiosk mode (fullscreen) pointing to localhost:5000
+    # Flags to suppress GPU errors and improve performance on Raspberry Pi
+    chromium-browser --kiosk \
+        --disable-gpu \
+        --disable-software-rasterizer \
+        --disable-dev-shm-usage \
+        --no-sandbox \
+        --disable-features=VizDisplayCompositor \
+        http://localhost:5000 &
+else
+    echo "No X server detected. Server is running at http://localhost:5000"
+    echo "Access the interface from a browser or run this script from the desktop environment."
+fi
