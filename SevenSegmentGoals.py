69769b9afc0750729256b576197b61f604ee4569
andreas riedmann <andreas.riedmann@htldornbirn.at>
2025-11-19 16:22:19 +0100

    try

--- SevenSegmentGoals.py
+++ SevenSegmentGoals.py
@@ -12,6 +12,15 @@ class SevenSegmentGoals:
         i2c = busio.I2C(board.SCL, board.SDA)
         self.segment1 = segments.Seg7x4(i2c, address=constant.SEVEN_SEGMENT_ADDRESS_GOALS_1)
         self.segment2 = segments.Seg7x4(i2c, address=constant.SEVEN_SEGMENT_ADDRESS_GOALS_2)
+        
+        # Set brightness (0.0 to 1.0)
+        self.segment1.brightness = 1.0
+        self.segment2.brightness = 1.0
+        
+        # Enable auto_write for immediate display updates
+        self.segment1.auto_write = True
+        self.segment2.auto_write = True
+        
         self.printToSegment(0,0)
         
 
@@ -19,6 +28,7 @@ class SevenSegmentGoals:
     def printToSegment(self, goals1, goals2):
         # Format the display string for segment1
         display_str = "{:2d}{:2d}".format(goals1, goals2)
+        print(f"Updating goal display to: {goals1}:{goals2} (display_str: {display_str})")
         
         # Clear and update segment1
         self.segment1.fill(0)
@@ -31,5 +41,7 @@ class SevenSegmentGoals:
         self.segment2.print(display_str)
         self.segment2.colon = True
         self.segment2.show()
+        
+        print("Goal display updated successfully")
     
    
