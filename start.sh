a3dc49349cdda97e158ba2c80a15adc86f318bdf
andreas riedmann <andreas.riedmann@htldornbirn.at>
2025-11-26 10:58:42 +0100

    merging

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~/Documents/kickerkasten/server_new || exit 1
+source ../venv/bin/activate
+exec "$SHELL"
---
+++ start_venv.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+cd ~/Documents/kickerkasten/
+source venv/bin/activate
