f6aaa30de813bbb45719815c1961209c9e1ceaf1
andreas riedmann <andreas.riedmann@htldornbirn.at>
2025-11-26 15:25:51 +0100

    added startupscript

---
+++ start_kiosk.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Wait for system to be ready
+sleep 10
+
+# Navigate to the project directory
+cd /home/pi/Documents/kickerkasten
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Start the Flask server in the background
+python3 -m server.server &
+
+# Wait for the server to start
+sleep 5
+
+# Start Chromium in kiosk mode
+DISPLAY=:0 chromium-browser --kiosk --incognito --disable-infobars --noerrdialogs --disable-session-crashed-bubble http://localhost:5000
