5d6beac3dcbb713db5d9efda41539b3806b2d689
andreas riedmann <andreas.riedmann@htldornbirn.at>
2025-11-26 11:25:18 +0100

    removed pygame

--- server/sound_manager.py
+++ server/sound_manager.py
@@ -1,7 +1,6 @@
 """
 Sound manager for playing game sounds
 """
-import pygame
 import os
 import config
 
@@ -10,14 +9,13 @@ class SoundManager:
     """Handles all game sounds"""
     
     def __init__(self):
-        pygame.mixer.init()
         
         # Load sound files
         sound_path = os.path.join(os.path.dirname(__file__), '..', 'sound')
         
-        self.goal_sound = pygame.mixer.Sound(os.path.join(sound_path, 'goal.ogg'))
-        self.start_sound = pygame.mixer.Sound(os.path.join(sound_path, 'start.ogg'))
-        self.nogoal_sound = pygame.mixer.Sound(os.path.join(sound_path, 'nogoal.ogg'))
+        self.goal_sound = None
+        self.start_sound = None
+        self.nogoal_sound = None
         
         print("Sound manager initialized")
         print(f"  Sound folder: {sound_path}")
